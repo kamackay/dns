b3e7da743449581d535f62eceaba02084441d164
Keith MacKay <kemackay@cisco.com>
2020-03-05 11:17:46 -0500

    Continue fleshing out dns lookups

--- server.go
+++ server.go
@@ -1,46 +1,77 @@
 package main
 
 import (
+	"github.com/bogdanovich/dns_resolver"
 	"github.com/miekg/dns"
+	"github.com/sirupsen/logrus"
 	"gitlab.com/kamackay/dns/logging"
+	"log"
 	"net"
 	"strconv"
+	"strings"
 )
 
-var domainsToAddresses map[string]string = map[string]string{
-	"google.com.": "1.2.3.4",
-	"cloudflare.com.": "1.1.1.1",
+type handler struct {
+	resolver *dns_resolver.DnsResolver
+	domains  map[string]string
 }
 
-type handler struct{}
+func (this *handler) getIp(domain string, logger *logrus.Logger) (string, error) {
+	address, ok := this.domains[domain]
+	if ok {
+		return address, nil
+	} else {
+		logger.Infof("Looking up %s", domain)
+		if ips, err := this.resolver.LookupHost(strings.TrimRight(domain, "."));
+			err != nil || len(ips) == 0 {
+			logger.Error(err)
+			return "", err
+		} else {
+			go func() {
+				// Add to cache
+
+			}()
+			return ips[0].String(), nil
+		}
+	}
+}
+
 func (this *handler) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
-	log := logging.GetLogger()
+	logger := logging.GetLogger()
 	msg := dns.Msg{}
 	msg.SetReply(r)
 	switch r.Question[0].Qtype {
 	case dns.TypeA:
 		msg.Authoritative = true
-		domain := msg.Question[0].Name
-		log.Infof("Request for name %s", domain)
-		address, ok := domainsToAddresses[domain]
-		if ok {
-			msg.Answer = append(msg.Answer, &dns.A{
-				Hdr: dns.RR_Header{ Name: domain, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 60 },
-				A: net.ParseIP(address),
-			})
+		for _, question := range msg.Question {
+			domain := question.Name
+			address, err := this.getIp(domain, logger)
+			logger.Infof("Request for domain '%s' --> %s", domain, address)
+			if err == nil {
+				msg.Answer = append(msg.Answer, &dns.A{
+					Hdr: dns.RR_Header{Name: domain, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 60},
+					A:   net.ParseIP(address),
+				})
+			}
 		}
 	}
 	w.WriteMsg(&msg)
 }
 
 func main() {
-	log := logging.GetLogger()
-	port := strconv.Itoa(53)
-	srv := &dns.Server{Addr: ":" + port, Net: "udp"}
-	srv.Handler = &handler{}
+	logger := logging.GetLogger()
+	logger.Infof("Starting...")
+	port := 53
+	srv := &dns.Server{Addr: ":" + strconv.Itoa(port), Net: "udp"}
+	srv.Handler = &handler{
+		resolver: dns_resolver.New([]string{"1.1.1.1"}),
+		domains: map[string]string{
+			"cloudflare.com.": "1.1.1.1",
+		},
+	}
 	if err := srv.ListenAndServe(); err != nil {
 		log.Fatalf("Failed to set udp listener %s\n", err.Error())
 	} else {
-		log.Infof("Connected to port %s", port)
+		logger.Infof("Started on Port %d", port)
 	}
 }
